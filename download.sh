42dd72b02d989045a874ae3659a01e42b40e8da2
d7x <36901198+d7x@users.noreply.github.com>
2023-06-22 16:50:39 +0300

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## linux bash threaded curl downloader ##
+## by d7x ##
+## d7x.promiselabs.net ##
+
+COUNTER=0 
+THREADS=40
+DELAY=10
+INPUT_FILENAME="urls"
+OUTPUT_DIRECTORY="downloaded"
+
+for i in $(cat ${INPUT_FILENAME}) 
+do
+        if [ $COUNTER -eq $THREADS ] 
+        then
+                echo "Reached threshold, sleeping for $DELAY seconds"
+                sleep $DELAY
+                COUNTER=0
+        fi
+        FILENAME=`basename $i`
+        echo $i >> eyewitness_local/urls_local.txt
+        curl -s -k -m 4 -f -o "${OUTPUT_DIRECTORY}/`basename $i`" $i -H "User-Agent: Mozilla/5.0" & 
+        # echo $COUNTER &
+        let COUNTER=COUNTER+1
+done
